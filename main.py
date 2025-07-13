8005cffff9189229d1aa872b98c680c846d5d8db
sagnikc395 <sagnikchatterjee607@gmail.com>
2025-07-14 00:31:49 +0530

    feat: generate binary

--- main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from vcs!")
-
-
-if __name__ == "__main__":
-    main()
---
+++ vcs.py
@@ -0,0 +1,5 @@
+def main():
+    print("welcome to the vcs")
+
+if __name__ == '__main__':
+    main()
