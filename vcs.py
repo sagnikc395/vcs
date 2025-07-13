6599f33fa81a8572ea1cfefdd12be183c223e9b0
sagnikc395 <sagnikchatterjee607@gmail.com>
2025-07-14 00:40:48 +0530

    feat: added supported cmds

--- vcs.py
+++ vcs.py
@@ -1,5 +1,61 @@
-def main():
+import argparse
+import configparser
+
+from datetime import datetime
+
+import grp,pwd 
+from fnmatch import fnmatch
+
+import hashlib
+
+from math import ceil
+
+import os
+
+
+import re
+
+import sys
+
+
+import zlib
+
+
+
+
+
+
+def main(argv=sys.argv[1:]):
     print("welcome to the vcs")
+    argparser = argparse.ArgumentParser(description="probably the stupidest content tracker")
+    argsubparsers = argparser.add_subparsers(title="Commands",dest="command")
+
+    argsubparsers.required = True
+    args = argparser.parse_args(argv)
+    match args.command:
+        case "add":
+            cmd_add(args)
+        case "cat-file":
+            cmd_cat_file(args)
+        case "check-ignore":
+            cmd_check_ignore(args)
+        case "checkout":
+            cmd_checkout(args)
+        case "commit":
+            cmd_commit(args)
+        case "hash-object":
+            cmd_hash_object(args)
+        case "init":
+            cmd_init(args)
+        case "log":
+            cmd_log(args)
+        case "ls-files":
+            cmd_ls_files(args)
+        case "ls-tree":
+            cmd_ls_tree(args)
+             
+
+    
 
 if __name__ == '__main__':
     main()
