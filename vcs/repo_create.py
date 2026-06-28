4f901631dd9f6d322c28fc8490d16d45fc025ad2
sagnikc395 <sagnikchatterjee607@gmail.com>
2026-06-28 13:03:52 -0400

    feat: implement repo find

--- vcs/repo_create.py
+++ vcs/repo_create.py
@@ -1,5 +1,23 @@
 # type: ignore
+import os 
 
+from .git_repo import GitRepository
 
 def repo_find(path=".", required=True):
-    pass
+    path = os.path.realpath(path)
+    
+    if os.path.isdir(os.path.join(path,".git")):
+        return GitRepository(path)
+    
+    # if we haven't returned, recurse in parent 
+    parent = os.path.realpath(os.path.join(path,".."))
+    
+    if parent == path:
+        # base case 
+        # no git 
+        if required:
+            raise Exception("no git directory")
+        else:
+            return None 
+        
+    return repo_find(parent,required)
